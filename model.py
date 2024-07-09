77e96bbad59b919c6cc4d8912536e5ac905516c6
wileyliao <wileyliao9552@gmail.com>
2024-07-09 09:01:52 +0800

    model related

---
+++ model.py
@@ -0,0 +1,35 @@
+from ultralytics import YOLO
+import multiprocessing
+
+def class_check():
+    model = YOLO('model_v1.pt')
+    for class_id, class_name in model.names.items():
+        print(f"ID: {class_id}, Class: {class_name}")
+
+def model_train():
+    # model storage path = my_project/test_run_01
+    multiprocessing.freeze_support()
+    model = YOLO('models/yolov8n.pt')
+    results = model.train(
+        data = 'data.yaml',
+        imgsz = 256,
+        epochs = 200,
+        patience = 10,
+        batch = 10,
+        project = 'my_project',
+        name = 'test_run_01'
+    )
+    return results
+
+def load_model(model_path):
+    """
+    1. 默認情況下，模型會在 CPU 上運行(model = YOLO(model_path))
+    2. 要在GPU上運行則要加上(model.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu')))
+    param
+        model_path:模型路徑
+    return
+        使用的模型
+    """
+    model = YOLO(model_path)
+    model.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+    return model
